046606ab76853e4eccafd9dc0a0b4c18ac07c72e
ilanesterenko <illuazagraenko12@gmail.com>
2023-11-19 11:27:44 +0200

    19.11.2023

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from users import api
+from users.api import create_issues
 from .exchange_rates import exchange_rates
+from users.api import issues_all
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("users/all", api.all),
-    path("users/create", api.create),
+    path("users/all", api.user_all),
+    path("users/create", api.create_user),
     path("exchange_rates/", exchange_rates),
+    path("create_issues/", create_issues),
+    path("issues_all/", issues_all),
 ]
--- src/users/api.py
+++ src/users/api.py
@@ -1,12 +1,12 @@
 import json
 from django.http import JsonResponse
-from .models import User
+from .models import User, Issues
 from django.db import models
 
 
-def all(request):
+def user_all(request):
     users = User.objects.all()
-    attrs = {"id", "email", "firstname", "last_name", "password", "role"}
+    attrs = {"id", "email", "first_name", "last_name", "password", "role"}
     results = []
 
     for user in users:
@@ -16,7 +16,7 @@ def all(request):
     return JsonResponse({"result": results})
 
 
-def create(request):
+def create_user(request):
     if request.method != "POST":
         raise NotImplementedError('Only POST requsts')
 
@@ -26,7 +26,35 @@ def create(request):
     if not user:
         raise Exception("Can not create user")
 
-    attrs = {"id", "email", "firstname", "last_name", "password", "role"}
+    attrs = {"id", "email", "first_name", "last_name", "password", "role"}
     payload = {attr: getattr(user, attr) for attr in attrs}
 
     return JsonResponse(payload)
+
+
+def create_issues(request):
+    if request.method != "POST":
+        raise NotImplementedError('Only POST requsts')
+
+    data = json.loads(request.body)
+    issues = Issues.objects.create(**data)
+
+    if not issues:
+        raise Exception("Can not create user")
+
+    attrs = {"title", "body", "junior_id", "senior_id", "status", }
+    payload = {attr: getattr(issues, attr) for attr in attrs}
+
+    return JsonResponse(payload)
+
+
+def issues_all(request):
+    issues = Issues.objects.all()
+    attrs = {"title", "body", "junior_id", "senior_id", "status",}
+    results = []
+
+    for issuess in issues:
+        payload = {attr: getattr(issuess, attr) for attr in attrs}
+        results.append(payload)
+
+    return JsonResponse({"result": results})
